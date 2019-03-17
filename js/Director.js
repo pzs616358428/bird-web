3ed106cd222926b9830faa865dd940513bece481
庞振硕 <616358428@qq.com>
2019-03-17 22:21:02 +0800

    调整代码结构

--- js/Director.js
+++ js/Director.js
@@ -1,14 +1,21 @@
 // 导演类,控制游戏的逻辑
-export class Director {
-    constructor() {
-        console.log(1)
-    }
+import {DataStore} from "./base/DataStore.js";
 
+export class Director {
 
     static getInstance() {
         if (!Director.instance) {
             Director.instance = new Director();
         }
         return Director.instance;
+    }
+
+    constructor() {
+        this.dataStore = DataStore.getInstance();
+    }
+
+    run() {
+        const backgroundSprite = this.dataStore.map.get('background');
+        backgroundSprite.draw();
     }
 }
--- js/base/Sprite.js
+++ js/base/Sprite.js
@@ -1,15 +1,17 @@
 // 精灵的基类,负责初始化精灵加载的资源和大小以及位置
+import {DataStore} from "./DataStore.js";
+
 export class Sprite {
 
-    constructor(ctx = null,
-                img = null,
+    constructor(img = null,
                 srcX = 0,
                 srcY = 0,
                 srcW = 0,
                 srcH = 0,
                 x = 0, y = 0,
                 width = 0, height = 0) {
-        this.ctx = ctx;
+        this.dataStore = DataStore.getInstance();
+        this.ctx = this.dataStore.ctx;
         this.img = img;
         this.srcX = srcX;
         this.srcY = srcY;
@@ -21,17 +23,29 @@ export class Sprite {
         this.height = height;
     }
 
-    draw() {
+    static getImage(key){
+        return DataStore.getInstance().res.get(key);
+    }
+
+    draw(img = this.img,
+         srcX = this.srcX,
+         srcY = this.srcY,
+         srcW = this.srcW,
+         srcH = this.srcH,
+         x = this.x,
+         y = this.y,
+         width = this.width,
+         height = this.height) {
         this.ctx.drawImage(
-            this.img,
-            this.srcX,
-            this.srcY,
-            this.srcW,
-            this.srcH,
-            this.x,
-            this.y,
-            this.width,
-            this.height
+            img,
+            srcX,
+            srcY,
+            srcW,
+            srcH,
+            x,
+            y,
+            width,
+            height
         );
     }
 
